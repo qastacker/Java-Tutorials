fe0e78fb76d56587c648775a2203565d75120309
Azhar <43946851+azharhot04@users.noreply.github.com>
2021-07-15 16:05:48 +0530

    Print words in new line

---
+++ JavaTutorials/src/J3_printInNewLine.java
@@ -0,0 +1,10 @@
+
+public class J3_printInNewLine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String br="Hello Programming"+"\n"+"I don't like you"+"\n"+"But I have to LEARN";
+System.out.println(br);
+	}
+
+}
