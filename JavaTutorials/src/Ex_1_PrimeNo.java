f0c9180d94650c16c755d4e53ce9911bd0f38a3e
Azhar <43946851+azharhot04@users.noreply.github.com>
2021-07-15 13:07:05 +0530

    Task 1 - To check number is prime or not

---
+++ JavaTutorials/src/Ex_1_PrimeNo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ex_1_PrimeNo {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/************* Prime Number **********/
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter the starting number : ");
+		int start = s.nextInt();
+		System.out.print("Enter the Ending number : ");
+		int end = s.nextInt();
+
+		
+			for (int i = start; i <= end; i++) {
+				if (isPrime(i)) {
+					System.out.println(i);
+				}
+			}
+	}
+
+	public static boolean isPrime(int i) {
+		for (int j = 2; j <= Math.sqrt(i); j++) {
+			if (i % j == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+
+}
