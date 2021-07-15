a4416f6483d04b378fbaeaf756c1614f9251bb3e
Azhar <43946851+azharhot04@users.noreply.github.com>
2021-07-15 16:12:04 +0530

    Print Output in reverse

---
+++ JavaTutorials/src/J4_Output.java
@@ -0,0 +1,24 @@
+
+public class J4_Output {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*****************************************************/
+		
+		int i=1;
+		  while (i<=10)
+		    {
+		    System.out.println(i);
+		    i+=2;
+		    }
+			
+		/*****************************************************/
+		int j=100;
+		  while (j<150)
+		    {
+		    System.out.println(j+1);
+		    j--;
+		    }
+	}
+
+}
