bfe5041b465c2211bff5edc9bffbe9e547704dc8
Azhar <43946851+azharhot04@users.noreply.github.com>
2021-07-15 17:44:17 +0530

    Print element is true based on first element

---
+++ JavaTutorials/src/J13.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class J13 {
+
+	@SuppressWarnings({ "resource" })
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String c1;
+		boolean c2;
+
+		Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter the string:");
+		c1 = s1.next();
+		Scanner s2 = new Scanner(System.in);
+		System.out.println("Enter the exception string:");
+		c2 = s2.nextBoolean();
+		if (c2 == true) {
+			System.out.println(c1.charAt(0));
+		} else
+			System.out.println(c1.charAt(c1.length() - 1));
+
+	}
+
+}
